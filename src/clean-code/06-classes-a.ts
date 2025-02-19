8676dbf18a46500713b6ab87af0585d3c8f23f9e
ImNotGabo <gabriel.duarte18@outlook.es>
2025-02-18 20:06:50 -0400

    Inheritance  issues

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,17 +1,58 @@
 (() => {
 	type Gender = 'M' | 'F';
 
+	// No responsabilidad unica
 	class Person {
-		public name: string;
-		public gender: Gender;
-		public birthdate: Date;
+		constructor(
+			public name: string,
+			public gender: Gender,
+			public birthdate: Date
+		) {}
+	}
 
-		constructor(name: string, gender: Gender, birthdate: Date) {
-			this.name = name;
-			this.gender = gender;
-			this.birthdate = birthdate;
+	class User extends Person {
+		public lastAccess: Date;
+		constructor(
+			public email: string,
+			public role: string,
+			name: string,
+			gender: Gender,
+			birthdate: Date
+		) {
+			super(name, gender, birthdate);
+			this.lastAccess = new Date();
+		}
+
+		checkCredentials() {
+			return true;
 		}
 	}
-	const newPerson = new Person('Gabriel', 'M', new Date('2002-10-06'));
+
+	class UserSettings extends User {
+		constructor(
+			public workingDirectory: string,
+			public lastOpenFolder: string,
+			email: string,
+			role: string,
+			name: string,
+			gender: Gender,
+			birthDate: Date
+		) {
+			super(email, role, name, gender, birthDate);
+		}
+	}
+	const newPerson = new Person('Gabriel', 'M', new Date('2002-10-07'));
 	console.log(newPerson);
+
+	const UserSettings1 = new UserSettings(
+		'/usr/home',
+		'/home',
+		'gabriel@example.com',
+		'Admin',
+		'Gabriel',
+		'M',
+		new Date('2025-02-18')
+	);
+
+	console.log({ UserSettings1 });
 })();
