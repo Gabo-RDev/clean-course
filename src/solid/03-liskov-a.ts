c5a0a9f82c9bd1c85dacfce93c5e5a457aed3ca7
ImNotGabo <gabriel.duarte18@outlook.es>
2025-03-24 16:20:45 -0400

    refactor: update printCarSeats function to accept Vehicle type and streamline seat printing for all car models

--- src/solid/03-liskov-a.ts
+++ src/solid/03-liskov-a.ts
@@ -1,28 +1,41 @@
-import { Tesla, Audi, Toyota, Honda } from './03-liskov-b';
+import { Tesla, Audi, Toyota, Honda, Vehicle, Ford } from './03-liskov-b';
 
 (() => {
-	const printCarSeats = (cars: (Tesla | Audi | Toyota | Honda)[]) => {
-		for (const car of cars) {
+	const printCarSeats = (cars: Vehicle[]) => {
+		/* 		for (const car of cars) {
 			if (car instanceof Tesla) {
-				console.log('Tesla', car.getNumberOfTeslaSeats());
+				console.log('Tesla', car.getNumbersOfSeats());
 				continue;
 			}
 			if (car instanceof Audi) {
-				console.log('Audi', car.getNumberOfAudiSeats());
+				console.log('Audi', car.getNumbersOfSeats());
 				continue;
 			}
 			if (car instanceof Toyota) {
-				console.log('Toyota', car.getNumberOfToyotaSeats());
+				console.log('Toyota', car.getNumbersOfSeats());
 				continue;
 			}
 			if (car instanceof Honda) {
-				console.log('Honda', car.getNumberOfHondaSeats());
+				console.log('Honda', car.getNumbersOfSeats());
 				continue;
 			}
-		}
+			if (car instanceof Ford) {
+				console.log('Ford', car.getNumbersOfSeats());
+				continue;
+			}
+		} */
+		cars.forEach((car) => {
+			console.log(car.constructor.name, car.getNumbersOfSeats());
+		});
 	};
 
-	const cars = [new Tesla(7), new Audi(2), new Toyota(5), new Honda(5)];
+	const cars = [
+		new Tesla(7),
+		new Audi(2),
+		new Toyota(5),
+		new Honda(5),
+		new Ford(2),
+	];
 
 	printCarSeats(cars);
 })();
